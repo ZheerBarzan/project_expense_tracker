8fc5ab0f1ca2a9aa7180163f931dfdb6010a0cfe
Zheer Barzan <zheerbarzan85@outlook.com>
2024-09-11 18:25:39 +0300

    adding the database and the required fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_expense_tracker/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
