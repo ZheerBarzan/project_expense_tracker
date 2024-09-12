68dcd132c6afcd507af6d5eb0556d37eede7deff
Zheer Barzan <zheerbarzan85@outlook.com>
2024-09-12 20:02:29 +0300

    adding the crud operations

--- lib/database/expense_database.dart
+++ lib/database/expense_database.dart
@@ -6,7 +6,7 @@ import 'package:project_expense_tracker/models/expense.dart';
 class ExpenseDatabase extends ChangeNotifier {
   static late Isar isar;
 
-  List<Expense> expenses = [];
+  List<Expense> _expenses = [];
 
   // setup
 
@@ -18,8 +18,46 @@ class ExpenseDatabase extends ChangeNotifier {
   }
 
   // getters
+  List<Expense> get getExpenses => _expenses;
 
   // operations
+
+  //create
+  Future<void> createExpense(Expense expense) async {
+    await isar.writeTxn(() async {
+      await isar.expenses.put(expense);
+    });
+
+    await readExpenses();
+  }
+
+  //read
+  Future<void> readExpenses() async {
+    List<Expense> expenses = await isar.expenses.where().findAll();
+
+    _expenses.clear();
+    _expenses.addAll(expenses);
+
+    notifyListeners();
+  }
+
+  //update
+
+  Future<void> updateExpense(int id, Expense expense) async {
+    expense.id = id;
+
+    await isar.writeTxn(() async {
+      await isar.expenses.put(expense);
+    });
+
+    await readExpenses();
+  }
+
+  //delete
+  Future<void> deleteExpense(int id) async {
+    await isar.writeTxn(() => isar.expenses.delete(id));
+    await readExpenses();
+  }
 
   // helpers
 }
